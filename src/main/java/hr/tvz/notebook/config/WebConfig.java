c1cebcbab335d42734781fc00260768d0d74ccf3
jelenabole <jelenabole@gmail.com>
2017-05-30 16:25:23 +0200

    - error interceptor fixed - deleted unnecessary files

--- src/main/java/hr/tvz/notebook/config/WebConfig.java
+++ src/main/java/hr/tvz/notebook/config/WebConfig.java
@@ -2,12 +2,8 @@ package hr.tvz.notebook.config;
 
 import java.util.Locale;
 
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
@@ -15,24 +11,8 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import hr.tvz.notebook.web.interceptor.AuthenticationInterceptor;
 
-//servlet-context.xml
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
-
-	@Bean
-	public EmbeddedServletContainerCustomizer containerCustomizer() {
-		return new EmbeddedServletContainerCustomizer() {
-			@Override
-			public void customize(ConfigurableEmbeddedServletContainer container) {
-				ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/error/401.html");
-				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/error/404.html");
-				ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,
-						"/error/500.html");
-				ErrorPage error403Page = new ErrorPage(HttpStatus.FORBIDDEN, "/error/403.html");
-				container.addErrorPages(error401Page, error403Page, error404Page, error500Page);
-			}
-		};
-	}
 
 	/**** LOCALE AND AUTHENTICATION INTERCEPTORS ****/
 
